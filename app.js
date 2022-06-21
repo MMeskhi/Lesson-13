7c207f5495494a0025a2fa549755393c345d162a
Mikheil Meskhi <mishameskhi1@gmail.com>
2022-06-21 11:22:33 +0400

    add script

---
+++ app.js
@@ -0,0 +1,24 @@
+const myName = "Mikheil Meskhi"; //string
+let myHobby = "listening to music"; //string
+const yearOfBirth = 1998; //number
+let currentYear = 2022; //number
+let student = true; //boolean
+const myUndefined = undefined; //undefined
+const myNull = null; //null
+
+//method 1
+console.log(
+  `My name is ${myName}, I am ${
+    currentYear - yearOfBirth
+  } years old and my hobby is ${myHobby}`
+);
+
+//method 2
+console.log(
+  "My name is " +
+    myName +
+    ", I am " +
+    (currentYear - yearOfBirth) +
+    " years old and my hobby is " +
+    myHobby
+);
